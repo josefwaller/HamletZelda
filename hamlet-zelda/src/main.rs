328b0a04d2e3a65d4730756f494f7ae3d2671399
josefwaller <josef@siriusapplications.com>
2016-10-18 12:47:07 -0700

    Created the main game loop

--- hamlet-zelda/src/main.rs
+++ hamlet-zelda/src/main.rs
@@ -1,3 +1,58 @@
+extern crate piston_window;
+
+use piston_window::{
+	
+	// used for creating the window
+	PistonWindow,
+	WindowSettings,
+	
+	// used in the game loop
+	RenderEvent,
+	UpdateEvent,
+	PressEvent,
+	ReleaseEvent,
+	
+	// the opengl
+	OpenGL
+};
+
 fn main() {
-    println!("Hello, world!");
+    
+	// initializes the OpenGL pointers
+	let opengl = OpenGL::V3_2;
+	
+	// creates the window
+	let mut window: PistonWindow = WindowSettings::new("The Legend Of Zelda: Hamlet", [1000, 800])
+	// sets to exit on esc
+	.exit_on_esc(true)
+	// sets opengl to use
+	.opengl(opengl)
+	// builds and unwraps the window
+	.build()
+	.unwrap();
+	
+	// cycles through window events
+	while let Some(e) = window.next() {
+		
+		// when a key is pressed
+		if let Some(k) = e.press_args() {
+			
+		}
+		
+		// when a key is released
+		if let Some(k) = e.release_args() {
+			
+		}
+		
+		// when the game should update
+		if let Some(u) = e.update_args() {
+			
+		}
+		
+		// when the game should render
+		if let Some(r) = e.render_args() {
+			
+		}
+		
+	}
 }
