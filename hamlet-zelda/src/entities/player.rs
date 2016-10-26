a548a66242fd98cae7b1d62fa6842148da8a400e
josefwaller <josef@siriusapplications.com>
2016-10-26 10:15:38 -0700

    Added position to Player as well as implementing HasBBox for Player

--- hamlet-zelda/src/entities/player.rs
+++ hamlet-zelda/src/entities/player.rs
@@ -9,13 +9,22 @@ use piston_window::{
 	RenderArgs
 };
 
+// see traits/has_bbox.rs
+use traits::has_bbox::HasBBox;
+
+// used for HasBBox
+use utilities::bbox::BBox;
+
 /*
 The player struct. Responsible for 
 moving, rendering and anything else 
 concerning the player.
 */
 pub struct Player {
-	
+	x: f64,
+	y: f64,
+	w: f64,
+	h: f64
 }
 
 impl Player {
@@ -26,7 +35,12 @@ impl Player {
 	Returns: A new Player object
 	*/
 	pub fn new() -> Player {
-		Player{}
+		Player{
+			x: 0.0,
+			y: 0.0,
+			w: 0.0,
+			h: 0.0
+		}
 	}
 	
 	/*
@@ -45,5 +59,22 @@ impl Player {
 	*/
 	pub fn render(&mut self, r: &RenderArgs) {
 		
+	}
+}
+
+impl HasBBox for Player {
+	
+	/*
+	See hes_bbox.rs
+	*/
+	fn get_bbox(&mut self) -> BBox {
+		
+		// returns a Bounding box of the player
+		BBox::new(
+			self.x,
+			self.y,
+			self.w,
+			self.h
+		)
 	}
 }
